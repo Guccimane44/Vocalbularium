bd5cc5458b04e98c3a02d95a85d762fbff1153a8
Yan Liu <guccimanederzweite@gmail.com>
2026-09-10 17:16:19 +0200

    Reset project to an empty AGENTS.md

--- scripts/chromium-sandbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-: "${VOCAB_CHROMIUM_EXECUTABLE:?A pinned Chromium executable is required}"
-: "${VOCAB_NETWORK_POLICY:?A loopback network policy is required}"
-exec /usr/bin/sandbox-exec -f "$VOCAB_NETWORK_POLICY" "$VOCAB_CHROMIUM_EXECUTABLE" "$@"
